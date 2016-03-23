4ca88614d83062142d45c7817592db43dc7af4ef
phreaker2600 <swwiley@gmail.com>
2016-03-23 00:40:28 +0000

    confirmation Im retarded

--- Recive/rx/rx.ino
+++ Recive/rx/rx.ino
@@ -25,9 +25,13 @@ unsigned int recive_byte() {
 	int i=0;
 	unsigned int r=0;
 	while(recive_bit()<2);
-	for(i=0;i<7;i++){
+	/*for(i=0;i<7;i++){
 		r=r|(recive_bit());
 		r=r<<1;
+	}*/
+	for(i=0;i<8;i++){
+		r=r|(recive_bit()<<i);
+		//r=r<<1;
 	}
 	return r;
 
